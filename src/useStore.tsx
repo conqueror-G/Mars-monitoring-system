3c610729e01d5f8df826451040b6296cff74c12e
conqueror-G <gitconquerorg@gmail.com>
2022-09-04 12:26:30 +0900

    refactor: useStore 작성 내용 제거
    useStore에서 리팩토링 아키테처 적용을 위해 작성 내용 제거

--- src/useStore.tsx
+++ src/useStore.tsx
@@ -1,36 +1,5 @@
-import { addItemToggle } from './stores/addItemToggle'
-import { deviceDetailData } from './stores/deviceDetailData'
-import { equipDetailDatas } from './stores/equipDetailDatas'
-import { listDatas } from './stores/listDatas'
-import { enteredUserInfo } from './stores/login/enteredUserInfo'
-import { pwVisibilityToggle } from './stores/login/pwVisibilityToggle'
-import { validMessageToggle } from './stores/login/validMessageToggle'
-import { mappingFilterOption } from './stores/mappingFilterOption'
-import { isSideNavToggle } from './stores/sideNav/isSideNavToggle'
-import { isTopNavIsToggle } from './stores/topNav/isTopNavIsToggle'
-import { messageId } from './stores/topNav/messageId'
-import { pathNumbers } from './stores/topNav/pathNumbers'
-import { usersInfo } from './stores/usersInfo'
-
 const useStore = () => {
-  return {
-    usersInfo,
-    //Login - usersInfo, enteredUserInfo, validMessageToggle, pwVisibilityToggle
-    enteredUserInfo,
-    validMessageToggle,
-    pwVisibilityToggle,
-    // TopNav - isToggle, messageId, pathNumbers
-    isTopNavIsToggle,
-    messageId,
-    pathNumbers,
-    // SideNav - isSideNavToggle
-    isSideNavToggle,
-    listDatas,
-    addItemToggle,
-    mappingFilterOption,
-    equipDetailDatas,
-    deviceDetailData,
-  }
+  return {}
 }
 
 export default useStore
