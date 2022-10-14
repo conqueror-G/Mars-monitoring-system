dc19e6e3265e0dd22e3d7f71eb50f09ac840d76a
conqueror-G <gitconquerorg@gmail.com>
2022-10-14 21:15:20 +0900

    style: 버튼 컴포넌트 스타일링

--- src/pages/Sign/SignPage.tsx
+++ src/pages/Sign/SignPage.tsx
@@ -1,4 +1,5 @@
-import { css } from '@emotion/react'
+import { css, useTheme } from '@emotion/react'
+import Button from 'src/components/Button'
 import Input from 'src/components/Input'
 
 const wrapperCss = {
@@ -9,20 +10,28 @@ const wrapperCss = {
 }
 
 export const SignPage = () => {
+  const theme = useTheme()
+
   return (
     <form>
-      <h1 css={{ fontSize: '1.25rem', marginBottom: '3rem' }}>Sign In</h1>
+      <h1 css={{ fontSize: '2rem', marginBottom: '3rem' }}>Sign In</h1>
 
       <div css={wrapperCss.input}>
-        <label htmlFor="inputId" css={{ fontSize: '1.125rem', marginBottom: '0.5rem' }}>
+        <label htmlFor="inputId" css={{ fontSize: '1.3rem', marginBottom: '0.5rem' }}>
           ID
         </label>
         <Input id="inputId" type="text" css={{ marginBottom: '1.5rem' }} />
 
-        <label htmlFor="passwordId" css={{ fontSize: '1.125rem', marginBottom: '0.5rem' }}>
+        <label htmlFor="passwordId" css={{ fontSize: '1.3rem', marginBottom: '0.5rem' }}>
           Password
         </label>
-        <Input id="passwordId" type="password" css={{ marginBottom: '1rem' }} />
+        <Input id="passwordId" type="password" css={{ marginBottom: '1.5rem' }} />
+
+        <Button
+          css={{ border: 0, fontSize: '1.25rem', fontWeight: 600, color: theme.color.Bg.bgDefault }}
+        >
+          Sign in
+        </Button>
       </div>
     </form>
   )
